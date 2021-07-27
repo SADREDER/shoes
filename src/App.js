96a9e61c3795a3199dfa40b717ee5c10916ba988
SANGROK KANG <sadreder@gmail.com>
2021-07-27 14:06:42 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import ShoesItem from './data.js';
 import './App.css';
+import Detail from './Detail.js';
 
 import { Link, Route, Switch } from 'react-router-dom';
 
@@ -17,8 +18,14 @@ function App() {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto'>
-              <Nav.Link href='#home'>Home</Nav.Link>
-              <Nav.Link href='#link'>Link</Nav.Link>
+              <Nav.Link>
+                {' '}
+                <Link to='/'>Home</Link>
+              </Nav.Link>
+              <Nav.Link>
+                {' '}
+                <Link to='/detail'>Detail</Link>
+              </Nav.Link>
               <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
                 <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
                 <NavDropdown.Item href='#action/3.2'>
@@ -37,43 +44,33 @@ function App() {
         </Container>
       </Navbar>
 
-      <Route exact path='/'>
-        <div className='background'>
-          <h1>20% Season Off</h1>
-          <p>This is a simple hero unit</p>
-          <p>
-            <Button variant='primary'>Primary</Button>{' '}
-          </p>
-        </div>
- 
-        <div className='container'>
-          <div className='row'>
-            {shoes.map((a, i) => {
-              return <Product key={i} shoes={a} />;
-            })}
+      <Switch>
+        <Route exact path='/'>
+          <div className='background'>
+            <h1>20% Season Off</h1>
+            <p>This is a simple hero unit</p>
+            <p>
+              <Button variant='primary'>Primary</Button>{' '}
+            </p>
           </div>
-        </div>
-      </Route>
 
-      <Route path='/detail'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-6'>
-              <img
-                src='https://codingapple1.github.io/shop/shoes1.jpg'
-                width='100%'
-                alt='shoes'
-              />
-            </div>
-            <div className='col-md-6 mt-4'>
-              <h4 className='pt-5'>상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className='btn btn-danger'>주문하기</button>
+          <div className='container'>
+            <div className='row'>
+              {shoes.map((a, i) => {
+                return <Product key={i} shoes={a} />;
+              })}
             </div>
           </div>
-        </div>{' '}
-      </Route>
+        </Route>
+
+        <Route path='/detail'>
+          <Detail />
+        </Route>
+
+        <Route path='/:id'>
+          <div>Anyting you want</div>
+        </Route>
+      </Switch>
     </div>
   );
 }
